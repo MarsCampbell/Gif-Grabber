e51cbc66c5d46d0e5cebdf4ecb4b36077ed06207
MarsCampbell <johnaj22@hotmail.com>
2018-08-24 23:17:52 -0400

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,76 @@
+var movies = ["Synecdoche New York", "Halloween", "Pulp Fiction", "Freaky Friday", "Home Alone",
+"Death Note", "Primer", "James and the Giant Peach", "Watership Down", "Total Recall",
+"The Little Mermaid", "Adaptation", "Batman", "Monsters Inc.", "Liar Liar",
+"Ace Ventura", "Vampires Kiss"];
+var gifsReturned = 10;
+var maxRating = "PG";
+
+function renderButtons(){
+	for(var i = 0; i < movies.length; i++) {
+		var newButton = $("<button>");
+		newButton.addClass("btn");
+		newButton.addClass("movies-button");
+		newButton.text(movies[i]);
+		$("#button-container").append(newButton);
+	}
+	$(".movies-button").unbind("click");
+
+	$(".movies-button").on("click", function(){
+		$(".gif-image").unbind("click");
+		$("#gif-container").empty();
+		$("#gif-container").removeClass("dotted-border");
+		populateGIFContainer($(this).text());
+	});
+
+}
+
+function addButton(show){
+	if(movies.indexOf(show) === -1) {
+		movies.push(show);
+		$("#button-container").empty();
+		renderButtons();
+	}
+}
+
+function populateGIFContainer(show){
+	$.ajax({
+		url: "https://api.giphy.com/v1/gifs/search?q=" + show + 
+		"&api_key=5qUPLo4L9eUOeLleIWXAoKVnSLapoy7N&rating=" + maxRating + "&limit=" + gifsReturned,
+		method: "GET"
+	}).then(function(response){
+		response.data.forEach(function(element){
+			newDiv = $("<div>");
+			newDiv.addClass("individual-gif-container");		
+			var newImage = $("<img src = '" + element.images.fixed_height_still.url + "'>");
+			newImage.addClass("gif-image");
+			newImage.attr("state", "still");
+			newImage.attr("still-data", element.images.fixed_height_still.url);
+			newImage.attr("animated-data", element.images.fixed_height.url);
+			newDiv.append(newImage);
+			$("#gif-container").append(newDiv);
+            newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
+		});
+		
+		$("#gif-container").addClass("dotted-border");
+		$(".gif-image").unbind("click");
+		$(".gif-image").on("click", function(){
+			if($(this).attr("state") === "still") {
+				$(this).attr("state", "animated");
+				$(this).attr("src", $(this).attr("animated-data"));
+			}
+			else {
+				$(this).attr("state", "still");
+				$(this).attr("src", $(this).attr("still-data"));
+			}
+		});
+	});
+}
+
+$(document).ready(function(){
+	renderButtons();
+	$("#submit").on("click", function(){
+		event.preventDefault();
+		addButton($("#movies").val().trim());
+		$("#movies").val("");
+	});
+});
